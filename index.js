2b0323623e04eedc4fc2513c44116ae13527b9cc
Atamas Ilya <atamas1lya@gmail.com>
2018-03-18 23:58:52 +0200

    Add chunks unrendering

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { Noise } from "node_modules/noisejs";
-
-const normalize = (val, min, max) => (val - min) / (max - min);
+import { Noise } from "./node_modules/noisejs";
 
 /**
  * Endless chunked map generator based on user position
@@ -24,12 +22,32 @@ class Generator {
     }
   }
 
-  _generateNoise(x, z) {
+  _generateNoise({ x, z }) {
     return this.noise.perlin2(x / 10, z / 10) * 100 + this.maxHeight / 2;
+  }
+
+  _unrenderChunksOutRange({ userX, userZ, renderDistance }) {
+    Object.keys(this.map).forEach(x => {
+      Object.keys(this.map[x]).forEach(z => {
+        const minX = userX - renderDistance;
+        const maxX = userX + renderDistance;
+
+        const minZ = userZ - renderDistance;
+        const maxZ = userZ + renderDistance;
+
+        if (x < minX || x > maxX) {
+          delete this.map[x];
+        } else if (z < minZ || z > maxZ) {
+          delete this.map[x][z];
+        }
+      });
+    });
   }
 
   updateMap({ userPosition, renderDistance }) {
     const [userX, userY, userZ] = userPosition;
+
+    this._unrenderChunksOutRange({ userX, userZ, renderDistance });
 
     for (let x = -renderDistance + userX; x < renderDistance + userX + 1; x++) {
       for (
@@ -40,12 +58,10 @@ class Generator {
         this._setMap({
           x,
           z,
-          value: this._generateNoise(x, z)
+          value: this._generateNoise({ x, z })
         });
       }
     }
-
-    console.log(this.map);
   }
 }
 
@@ -58,6 +74,19 @@ const generator = new Generator({
 for (let i = 0; i < 5; i++) {
   generator.updateMap({
     userPosition: [i, 0, i],
-    renderDistance: 0
+    renderDistance: 1
   });
 }
+
+const generator2 = new Generator({
+  seed: 1,
+  minHeight: 0,
+  maxHeight: 256
+});
+
+generator2.updateMap({
+  userPosition: [4, 0, 4],
+  renderDistance: 1
+});
+
+console.log(generator.map, generator2.map);
